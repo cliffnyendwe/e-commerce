6aab3c8f3548c818c39be3327e5f04c9ad503f3d
cliffnyendwe <cliffnyendwe2018@gmail.com>
2020-07-28 14:59:28 +0300

    secret key

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -34,6 +34,3 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
-STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
-STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
