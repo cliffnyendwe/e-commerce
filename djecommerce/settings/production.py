ecfa92e160ea52f1d1afeab27e2f1c82e54c115a
cliffnyendwe <cliffnyendwe2018@gmail.com>
2020-07-29 10:52:46 +0300

    changes

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -32,4 +32,4 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', 'https://craftshaven.herokuapp.com/', cast=Csv())
